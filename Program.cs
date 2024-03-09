63c6df4c62f05a47ee9d46454814b20ad268f340
ZussiMane <igoryunja@gmail.com>
2024-03-09 16:37:40 +0300

    Добавил решение задачи

--- Program.cs
+++ Program.cs
@@ -6,3 +6,18 @@
 // a = 9, b = -3 => да 
 // a = -3, b = 9 => нет
 
+Console.WriteLine("Введите первое число: ");
+// string num1str = Console.ReadLine();
+int num1 = Convert.ToInt32(Console.ReadLine()); // конвертировали тип "string" в 32-битное число, переменная int занимает в памяти 32 бита(4байта, в каждом байте 8бит=32бита)(диапозон типа данных int 2^32)
+
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 * num1 == num2)
+{
+    Console.WriteLine("Первое число является квадратом втрого числа");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
